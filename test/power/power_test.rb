c13f09db2a93ac21a202e1f616b888f79a2b23b5
Michael Lewandowski <milewgit@gmail.com>
2014-12-18 16:07:37 -0500

    Set up testing infrastructure

---
+++ test/power/power_test.rb
@@ -0,0 +1,7 @@
+require_relative '../test_helper'
+
+describe 'tests' do
+  it 'works' do
+    'works'.must_equal 'works'
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+require 'minitest/autorun'
+require 'mocha/setup'         # must be after require 'minitest/autorun'
+
+require_relative '../lib/power'
