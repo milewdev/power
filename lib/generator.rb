9b2138da832948e0cd9df340adff41506c2b099d
Michael Lewandowski <milewgit@gmail.com>
2014-12-19 16:28:08 -0500

    Extract method Generator.generate_new_numbers

--- lib/generator.rb
+++ lib/generator.rb
@@ -1,16 +1,22 @@
 # TODO: need a better name
 class Generator
-  def self.generate(set)
-    additions = Set.new
-    set.each do |a|
-      set.each do |b|
-        additions.add(a + b)
-      end
-    end
-    additions = (additions - set)
+  def self.generate(set)                    # [1,2]
+    additions = generate_new_numbers(set)   # [2,3,4]
+    additions = (additions - set)           # [3,4]
     generated_sets = additions.map do |a|
       Set.new(set).add(a).to_a.sort
     end
+  end
+
+  def self.generate_new_numbers(list)   # [1,2]
+    new_numbers = Set.new
+    list.each do |i|                    # i = 1,2
+      list.each do |j|                  # j = 1,2
+        new_number = i + j              # 1+1=2, 1+2=3, 2+1=3, 2+2=4
+        new_numbers.add(new_number)     # {2,3,4}  new_numbers a set, so 3 only added once
+      end
+    end
+    new_numbers.to_a
   end
 
   def self.generate_all(sets)
