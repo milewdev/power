5250faf0bead0474239449d20d8741804f561e14
Michael Lewandowski <milewgit@gmail.com>
2014-12-19 17:16:00 -0500

    Add Array#set_add so that we don't need Set

--- lib/generator.rb
+++ lib/generator.rb
@@ -1,33 +1,42 @@
+# TODO: use refinements
+class Array
+  def set_add(o)
+    self.push(o) unless self.include?(o)
+    self
+  end
+end
+
+
 # TODO: need a better name
 class Generator
   def self.generate(set)                    # [1,2]
     additions = generate_new_numbers(set)   # [2,3,4]
     additions = (additions - set)           # [3,4]
-    generated_sets = additions.map do |a|
-      Set.new(set).add(a).to_a.sort
+    additions.map do |a|
+      set.dup.set_add(a).sort
     end
   end
 
-  def self.generate_new_numbers(list)   # [1,2]
-    new_numbers = Set.new
-    list.each do |i|                    # i = 1,2
-      list.each do |j|                  # j = 1,2
-        new_number = i + j              # 1+1=2, 1+2=3, 2+1=3, 2+2=4
-        new_numbers.add(new_number)     # {2,3,4}  new_numbers a set, so 3 only added once
+  def self.generate_new_numbers(list)       # [1,2]
+    new_numbers = []
+    list.each do |i|                        # i = 1,2
+      list.each do |j|                      # j = 1,2
+        new_number = i + j                  # 1+1=2, 1+2=3, 2+1=3, 2+2=4
+        new_numbers.set_add(new_number)     # {2,3,4}  new_numbers a set, so 3 only added once
       end
     end
-    new_numbers.to_a
+    new_numbers
   end
 
   def self.generate_all(sets)
-    result = Set.new
+    result = []
     sets.each do |set|
       additions = Generator.generate(set)
       additions.each do |addition|
-        result.add(addition)
+        result.set_add(addition)
       end
     end
-    result.to_a.sort
+    result.sort
   end
 
   def self.find(n)
