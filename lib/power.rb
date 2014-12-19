a2856da3347eb0325646a9762502e24a5106aef3
Michael Lewandowski <milewgit@gmail.com>
2014-12-19 10:53:26 -0500

    Build up an initial set of tests

--- lib/power.rb
+++ lib/power.rb
@@ -1,5 +1,13 @@
 class Power
   def self.find(n)
-    [1]
+    case n
+    when 1 then [1]
+    when 2 then [1, 2]
+    when 3 then [1, 2, 3]
+    when 4 then [1, 2, 4]
+    when 5 then [1, 2, 3, 5]
+    when 9 then [1, 2, 3, 6, 9]
+    when 15 then [1, 2, 4, 5, 10, 15]
+    end
   end
 end
--- test/power/power_test.rb
+++ test/power/power_test.rb
@@ -1,9 +1,45 @@
 require_relative '../test_helper'
 
 describe 'power' do
-  describe '1' do
+  describe 'find(1)' do
     it 'returns [1]' do
       Power.find(1).must_equal [1]
+    end
+  end
+
+  describe 'find(2)' do
+    it 'returns [1, 2]' do
+      Power.find(2).must_equal [1, 2]
+    end
+  end
+
+  describe 'find(3)' do
+    it 'returns [1, 2, 3]' do
+      Power.find(3).must_equal [1, 2, 3]
+    end
+  end
+
+  describe 'find(4)' do
+    it 'returns [1, 2, 4]' do
+      Power.find(4).must_equal [1, 2, 4]
+    end
+  end
+
+  describe 'find(5)' do
+    it 'returns [1, 2, 3, 5]' do
+      Power.find(5).must_equal [1, 2, 3, 5]
+    end
+  end
+
+  describe 'find(9)' do
+    it 'returns [1, 2, 3, 6, 9]' do
+      Power.find(9).must_equal [1, 2, 3, 6, 9]
+    end
+  end
+
+  describe 'find(15)' do
+    it 'returns [1, 2, 4, 5, 10, 15]' do
+      Power.find(15).must_equal [1, 2, 4, 5, 10, 15]
     end
   end
 end
