24133fdbedd412099511c3826443133130499298
Michael Lewandowski <milewgit@gmail.com>
2014-12-20 00:35:32 -0500

    Combine tests to reduce bloat

--- test/power/power_test.rb
+++ test/power/power_test.rb
@@ -1,46 +1,28 @@
 require_relative '../test_helper'
 
-describe 'power' do
-  describe 'find(1)' do
-    it 'returns [1]' do
-      Power.find(1).must_equal [1]
-    end
-  end
 
-  describe 'find(2)' do
-    it 'returns [1, 2]' do
-      Power.find(2).must_equal [1, 2]
-    end
+describe 'Power.find(n)' do
+  it 'returns a smallest set of multiplications required to raise any number to the nth power' do
+    Power.find(1).must_equal [1]
+    Power.find(2).must_equal [1,2]
+    Power.find(3).must_equal [1,2,3]
+    Power.find(4).must_equal [1,2,4]
+    Power.find(5).must_equal [1,2,3,5]
+    Power.find(9).must_equal [1,2,3,6,9]
+    Power.find(15).must_equal [1,2,3,5,10,15]
   end
+end
 
-  describe 'find(3)' do
-    it 'returns [1, 2, 3]' do
-      Power.find(3).must_equal [1, 2, 3]
-    end
-  end
 
-  describe 'find(4)' do
-    it 'returns [1, 2, 4]' do
-      Power.find(4).must_equal [1, 2, 4]
-    end
-  end
-
-  describe 'find(5)' do
-    it 'returns [1, 2, 3, 5]' do
-      Power.find(5).must_equal [1, 2, 3, 5]
-    end
-  end
-
-  describe 'find(9)' do
-    it 'returns [1, 2, 3, 6, 9]' do
-      Power.find(9).must_equal [1, 2, 3, 6, 9]
-    end
-  end
-
-  describe 'find(15)' do
-    it 'returns [1, 2, 4, 5, 10, 15]' do
-      Power.find(15).must_equal [1, 2, 3, 5, 10, 15]
-    end
+describe 'Power.find_all(n)' do
+  it 'returns all smallest sets of multiplications required to raise any number to the nth power' do
+    Power.find_all(1).must_equal [[1]]
+    Power.find_all(2).must_equal [[1,2]]
+    Power.find_all(3).must_equal [[1,2,3]]
+    Power.find_all(4).must_equal [[1,2,4]]
+    Power.find_all(5).must_equal [[1,2,3,5], [1,2,4,5]]
+    Power.find_all(9).must_equal [[1,2,3,6,9], [1,2,4,5,9], [1,2,4,8,9]]
+    Power.find_all(15).must_equal [[1,2,3,5,10,15], [1,2,3,6,9,15], [1,2,3,6,12,15], [1,2,4,5,10,15]]
   end
 end
 
@@ -82,78 +64,6 @@ describe 'generator' do
   describe 'generate_all_next_power_sets([[1], [1,2]])' do
     it 'returns [[1,2], [1,2,3], [1,2,4]]' do
       Power.generate_all_next_power_sets([[1], [1,2]]).must_equal [[1,2], [1,2,3], [1,2,4]]
-    end
-  end
-end
-
-
-describe 'generator' do
-  describe 'find(1)' do
-    it 'returns [1]' do
-      Power.find(1).must_equal [1]
-    end
-  end
-
-  describe 'find(2)' do
-    it 'returns [1,2]' do
-      Power.find(2).must_equal [1,2]
-    end
-  end
-
-  describe 'find(3)' do
-    it 'returns [1,2,3]' do
-      Power.find(3).must_equal [1,2,3]
-    end
-  end
-
-  describe 'find(4)' do
-    it 'returns [1,2,4]' do
-      Power.find(4).must_equal [1,2,4]
-    end
-  end
-
-  describe 'find(5)' do
-    it 'returns [1,2,3,5]' do
-      Power.find(5).must_equal [1,2,3,5]
-    end
-  end
-end
-
-
-describe 'generator' do
-  describe 'find_all(1)' do
-    it 'returns [[1]]' do
-      Power.find_all(1).must_equal [[1]]
-    end
-  end
-
-  describe 'find_all(2)' do
-    it 'returns [[1,2]]' do
-      Power.find_all(2).must_equal [[1,2]]
-    end
-  end
-
-  describe 'find_all(3)' do
-    it 'returns [[1,2,3]]' do
-      Power.find_all(3).must_equal [[1,2,3]]
-    end
-  end
-
-  describe 'find_all(4)' do
-    it 'returns [[1,2,4]]' do
-      Power.find_all(4).must_equal [[1,2,4]]
-    end
-  end
-
-  describe 'find_all(5)' do
-    it 'returns [[1,2,3,5], [1,2,4,5]]' do
-      Power.find_all(5).must_equal [[1,2,3,5], [1,2,4,5]]
-    end
-  end
-
-  describe 'find_all(15)' do
-    it 'returns [[1,2,3,5,10,15], [1,2,3,6,9,15], [1,2,3,6,12,15], [1,2,4,5,10,15]]' do
-      Power.find_all(15).must_equal [[1,2,3,5,10,15], [1,2,3,6,9,15], [1,2,3,6,12,15], [1,2,4,5,10,15]]
     end
   end
 end
