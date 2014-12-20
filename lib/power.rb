7ee67957d12204e34e3f6447304804b0549b7c4a
Michael Lewandowski <milewgit@gmail.com>
2014-12-20 01:18:46 -0500

    Remove require's as there is only one source file

--- lib/power.rb
+++ lib/power.rb
@@ -1,8 +1,3 @@
-# 'require' all files except ourselves.
-me = File.absolute_path(__FILE__)
-Dir.glob(File.dirname(me) + '/**/*.rb') { |fn| require fn unless fn == me }
-
-
 class Power
 
   def self.find(n)
