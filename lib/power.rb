5672f93809157fb1c47b78851f07ab834ed079d0
Michael Lewandowski <milewgit@gmail.com>
2014-12-19 21:52:54 -0500

    Source code formatting: adjust white space, indentation

--- lib/power.rb
+++ lib/power.rb
@@ -2,11 +2,13 @@
 me = File.absolute_path(__FILE__)
 Dir.glob(File.dirname(me) + '/**/*.rb') {|fn| require fn unless fn == me }
 
+
 class Array
   def set_add(o)
     self.push(o) unless self.include?(o)
   end
 end
+
 
 class Power
   def self.find(n)
@@ -41,30 +43,30 @@ class Power
     result.sort
   end
 
-  def self.generate_next_power_sets(power_set)          # [1,2]
-    next_powers = generate_next_powers(power_set)       # [1,2] => [3,4]
-    build_power_sets(power_set, next_powers)            # [ [1,2,3], [1,2,4] ]
+  def self.generate_next_power_sets(power_set)        # [1,2]
+    next_powers = generate_next_powers(power_set)     # [1,2] => [3,4]
+    build_power_sets(power_set, next_powers)          # [ [1,2,3], [1,2,4] ]
   end
 
-  def self.generate_next_powers(power_set)              # [1,2]
+  def self.generate_next_powers(power_set)            # [1,2]
     next_powers = []
-    power_set.each do |power1|                          # power1 = 1,2
-      power_set.each do |power2|                        # power2 = 1,2
-        next_power = power1 + power2                    # 1+1=2, 1+2=3, 2+1=3, 2+2=4
-        next_powers.set_add(next_power)                 # [2,3,4]  note: set_add drops second 3
+    power_set.each do |power1|                        # power1 = 1,2
+      power_set.each do |power2|                      # power2 = 1,2
+        next_power = power1 + power2                  # 1+1=2, 1+2=3, 2+1=3, 2+2=4
+        next_powers.set_add(next_power)               # [2,3,4]  note: set_add drops second 3
       end
     end
-    next_powers - power_set                             # [2,3,4] - [1,2] = [3,4]
+    next_powers - power_set                           # [2,3,4] - [1,2] = [3,4]
   end
 
-  def self.build_power_sets(power_set, next_powers)     # [1,2], [3,4]
+  def self.build_power_sets(power_set, next_powers)   # [1,2], [3,4]
     next_powers.map do |next_power|
       build_power_set(power_set, next_power)
-    end                                                 # [1,2, 3] and [1,2, 4]
+    end                                               # [1,2, 3] and [1,2, 4]
   end
 
-  def self.build_power_set(power_set, next_power)       # [1,2,4], 3
-    power_set                                           # [1,2,4]
+  def self.build_power_set(power_set, next_power)     # [1,2,4], 3
+    power_set                                         # [1,2,4]
     .dup                                              # [1,2,4]
     .set_add(next_power)                              # [1,2,4,3]  note: set_add drops duplicates
     .sort                                             # [1,2,3,4]
